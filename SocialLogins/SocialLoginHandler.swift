190484c4f2be1f3207a4926d187cedd3543f6111
sivasonai <sivasonai@trioangle.com>
2021-10-25 15:29:11 +0530

    inital Project Setup -> Karuppasamy

---
+++ SocialLogins/SocialLoginHandler.swift
@@ -0,0 +1,14 @@
+//
+//  SocialLoginHandler.swift
+//  SocialLogins
+//
+//  Created by Trioangle on 25/10/21.
+//
+
+import Foundation
+
+open
+class SocialLoginsHandler : NSObject {
+    open static let shared = SocialLoginsHandler()
+    
+}
