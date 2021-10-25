9712b919389c4a7e3c4091ffddda8ddfab4994a9
sivasonai <sivasonai@trioangle.com>
2021-10-25 16:13:28 +0530

    Sample Google Login Code added -> Karuppasamy

--- SocialLogins/SocialLoginHandler.swift
+++ SocialLogins/SocialLoginHandler.swift
@@ -6,9 +6,53 @@
 //
 
 import Foundation
+import GoogleSignIn
+import FBSDKLoginKit
 
 open
 class SocialLoginsHandler : NSObject {
-    open static let shared = SocialLoginsHandler()
+    public static let shared = SocialLoginsHandler()
     
+    public
+    func handleGoogle(url: URL) -> Bool {
+        let handled = GIDSignIn.sharedInstance.handle(url)
+        return handled
+    }
+    
+    public
+    func doGoogleLogin(VC: UIViewController,
+                       clientID : String,
+                       completion: @escaping (Result<GIDGoogleUser,Error>) -> Void) {
+        let config : GIDConfiguration = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.signIn(with: config,
+                                        presenting: VC) { user, error in
+            if error != nil || user == nil {
+                guard let error = error else { return }
+                completion(.failure(error))
+            } else {
+                guard let user = user else { return }
+                completion(.success(user))
+            }
+        }
+    }
+    
+    public
+    func doGoogleSignOut() {
+        GIDSignIn.sharedInstance.signOut()
+    }
+    
+    public
+    func doGogleRelogin(completion: @escaping (Result<GIDGoogleUser,Error>) -> Void) {
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+                // Show the app's signed-out state.
+                guard let error = error else { return }
+                completion(.failure(error))
+            } else {
+                guard let user = user else { return }
+                completion(.success(user))
+                // Show the app's signed-in state.
+            }
+        }
+    }
 }
